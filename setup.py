83851795e3ae633c3043b755885a84557db63fdc
FamALouiz <fam@awadlouis.com>
2025-08-14 16:03:07 +0300

    feat: updated setup with new node

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ package_name = 'obj_detection'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='2.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -20,18 +20,14 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Salma Tarek',
-    maintainer_email='sosototo427@gmail.com',
+    maintainer='Fam Shihata',
+    maintainer_email='fam@awadlouis.com',
     description='LiDAR-based object detection and clustering for F1TENTH autonomous racing',
     license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'lidar_obj_detection_node_v1 = lidar_obj_detection.lidar_sub:main',
-            'lidar_obj_detection_node_v2 = lidar_obj_detection.lidar_subscriber_salma:main',
-            "lidar_cluster_exe = lidar_obj_detection.lidar_cluster_node:main",
-            "lidar_obj_detection_node = lidar_obj_detection.new_object_detector:main",
-            "lidar_obj_detection_node_v3 = lidar_obj_detection.lidar_sub_modi:main",
+            "lidar_obj_detection_node = lidar_obj_detection.lidar_obj_detection_node:main",
         ],
     },
 )
